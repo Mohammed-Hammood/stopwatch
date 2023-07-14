516fcd99acac14c171c8e63c8af7d8ce7f2b9c82
Mohammed <iraqiProfessionalDesigner@gmail.com>
2023-07-14 18:32:01 +0700

    first commit

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -3,17 +3,17 @@ import React, { createContext, useState } from "react";
 import { ThemeContextType } from "types";
 import { LocalStorage } from "utils";
 
+const isBrowser = typeof window !== 'undefined'? true : false; 
 
 export const ThemeContext = createContext<null | ThemeContextType>(null);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-    const ls = new LocalStorage();
-    const [theme, setTheme] = useState<"dark" | "light">(ls.getTheme());
+    const [theme, setTheme] = useState<"dark" | "light">(isBrowser ? new LocalStorage().getTheme(): "dark");
 
     const themeToggle = (): void => {
         const newTheme  = theme === 'dark' ? "light" : "dark"; 
         setTheme(newTheme);
-        ls.setTheme(newTheme);
+        new LocalStorage().setTheme(newTheme);
     }
 
     return (
